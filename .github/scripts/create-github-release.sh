46109e1a82a779404c04d2e06ad7c5ebed90fef3
Daniel Maddison <dmaddison@icloud.com>
2021-07-22 23:08:10 +1000

    Refactored GitHub Workflows

--- .github/scripts/create-github-release.sh
+++
@@ -1,2 +0,0 @@
-curl -v -s -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type:application/json" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases" -d "{ \"tag_name\": \"${GITHUB_TAG}\", \"target_commitish\": \"${GITHUB_SHA}\", \"name\": \"${GITHUB_TAG}\", \"body\": \"Release version ${GITHUB_TAG}\", \"draft\": false, \"prerelease\": false }"
-
